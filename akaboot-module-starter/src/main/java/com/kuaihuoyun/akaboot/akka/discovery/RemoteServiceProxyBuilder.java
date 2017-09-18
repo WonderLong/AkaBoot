9329462cdb6e84d4dd4bb6102b39159add953f8d
ericchen <ericchen1984@126.com>
2017-09-18 14:24:32 +0800

    服务注册

---
+++ akaboot-module-starter/src/main/java/com/kuaihuoyun/akaboot/akka/discovery/RemoteServiceProxyBuilder.java
@@ -0,0 +1,12 @@
+package com.kuaihuoyun.akaboot.akka.discovery;
+
+import com.kuaihuoyun.akaboot.remote.client.RemoteServiceClient;
+
+public class RemoteServiceProxyBuilder {
+
+    public static <T> T createInstance(Class<T> clazz, RemoteServiceClient client){
+        //todo build target object upon remote service
+        return null;
+    }
+
+}
---
+++ akaboot-remote/src/main/java/com/kuaihuoyun/akaboot/remote/client/RemoteServiceClient.java
@@ -0,0 +1,7 @@
+package com.kuaihuoyun.akaboot.remote.client;
+
+/**
+ * 远程服务客户端，建立底层服务链接
+ */
+public interface RemoteServiceClient {
+}
